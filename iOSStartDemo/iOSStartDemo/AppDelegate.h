cb64f23cd5f3b2aa124b137d0d6e834c3250cf2f
qiufu <chenxuan.cx@alibaba-inc.com>
2016-02-12 22:12:09 +0800

    Edit ignore

---
+++ iOSStartDemo/iOSStartDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSStartDemo
+//
+//  Created by qiufu on 2/12/16.
+//  Copyright © 2016 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSStartDemo/iOSStartDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSStartDemo
+//
+//  Created by qiufu on 2/12/16.
+//  Copyright © 2016 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
