a127fac1f369adfa84d5e20f114498dff7df2e08
qiufu <chenxuan.cx@alibaba-inc.com>
2016-02-13 15:44:50 +0800

    Add pods.

--- iOSStartDemo/iOSStartDemo/STMainViewController.m
+++ iOSStartDemo/iOSStartDemo/STMainViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "STMainViewController.h"
+#import <Masonry/Masonry.h>
+#import <SVProgressHUD/SVProgressHUD.h>
 
 @interface STMainViewController ()
 
@@ -31,34 +33,19 @@
     [helloButton setTitle:@"Hello" forState:UIControlStateNormal];
     [helloButton addTarget:self action:@selector(onHelloButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:helloButton];
-    //// Layout with frame.
-    //[helloButton setFrame:CGRectMake(0, 0, 60, 40)];
-    //helloButton.center = self.view.center;
-    //// Layout with constraint.
-    helloButton.translatesAutoresizingMaskIntoConstraints = NO; // If you want to use Auto Layout to dynamically calculate the size and position of your view, you must set this property to NO.
-    [self.view addConstraints:@[
-        [NSLayoutConstraint constraintWithItem:helloButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:60.0],
-        [NSLayoutConstraint constraintWithItem:helloButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:40.0],
-        [NSLayoutConstraint constraintWithItem:helloButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0],
-        [NSLayoutConstraint constraintWithItem:helloButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0]
-    ]];
+    [helloButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(@60.0);
+        make.height.equalTo(@40.0);
+        make.center.equalTo(self.view);
+    }];
 }
 
 
 #pragma mark - Action
 - (void)onHelloButtonClicked:(id)sender {
     NSLog(@"Hello, world!");
-
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Hello" message:@"Hello, world!" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-        NSLog(@"Cancle Action");
-    }];
-    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        NSLog(@"OK Action");
-    }];
-    [alertController addAction:cancelAction];
-    [alertController addAction:okAction];
-    [self presentViewController:alertController animated:YES completion:nil];
+    
+    [SVProgressHUD showSuccessWithStatus:@"Hello, world!" maskType:SVProgressHUDMaskTypeBlack];
 }
 
 @end
