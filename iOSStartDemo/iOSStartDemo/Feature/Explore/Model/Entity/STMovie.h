2b98889ed3b7a8a7b8909d2d0268f218fd03b0a3
SamirChen <sheng4893166@gmail.com>
2016-02-18 18:14:38 +0800

    Update.

--- iOSStartDemo/iOSStartDemo/Feature/Explore/Model/Entity/STMovie.h
+++ iOSStartDemo/iOSStartDemo/Feature/Explore/Model/Entity/STMovie.h
@@ -10,6 +10,7 @@
 
 @interface STMovie : NSObject
 
+@property (assign, nonatomic) int64_t rowid;
 @property (copy, nonatomic) NSString *name;
 @property (copy, nonatomic) NSString *year;
 @property (copy, nonatomic) NSString *synopsis;
